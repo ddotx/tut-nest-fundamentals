10507b3665b9db2eb43f2160d1304e2e46b3a38e
dome_dexpc <domemacintosh@gmail.com>
2024-08-07 02:52:01 +0700

    finish schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,22 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
+    }),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database host
+      host: process.env.DATABASE_HOST, // database host -> localhost
+      port: +process.env.DATABASE_PORT, // database host -> 5432
       username: 'postgres', // username
       password: 'pass123', // user password
       database: 'postgres', // name of our database,
